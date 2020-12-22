672cfdd828a55b67d0dfe8d077a5b7ce870bea2c
Asim Habib <de.asimhabib@gmail.com>
2020-12-22 17:44:39 +0500

    Made a correction ->(pop() the remaining operators on stack using while loop)

--- src/intopos/InToPos.java
+++ src/intopos/InToPos.java
@@ -30,28 +30,30 @@ public class InToPos
     public String inToPos(String exp)
     {
         Stack<Character> stack=new Stack<>();
-        String result="";
+        String postFix="";
         for (int i = 0; i < exp.length(); i++) 
         {
            char c=exp.charAt(i);
            
            if(Character.isLetterOrDigit(c))
-               result+=c;
+               postFix+=c;
            else if (c=='(')
                stack.push(c);
            else if(c==')')
            {
                while (!stack.isEmpty() && stack.peek()!='(') 
-                   result+=stack.pop();
+                   postFix+=stack.pop();
                stack.pop();
            } 
            else
            {
                while(!stack.isEmpty() && Prec(c)<=Prec(stack.peek()))
-                   result+=stack.pop();
+                   postFix+=stack.pop();
                stack.push(c);
            }
         }
-        return result;
+        while(!stack.isEmpty())
+            postFix+=stack.pop();
+        return postFix;
     }
 }
